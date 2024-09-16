fd3ffd99d10a2d2e576079ec860225fb0d700baf
Jeremy Myers <inernestmusic@gmail.com>
2024-09-16 16:21:10 -0400

    Add layer control

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,65 @@
 import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { Circle, LayersControl, MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { latLng, latLngBounds } from 'leaflet';
 import { mapOptions, SERVER } from './configs/settings';
+import { Band, MapMetadataResponse, MapResponse } from './types/maps';
 
 function App() {
-  const [maps, setMaps] = useState(undefined);
-  const [mapsMetadata, setMapsMetadata] = useState(undefined);
+  const [maps, setMaps] = useState<undefined | MapResponse[]>(undefined);
+  const [mapsMetadata, setMapsMetadata] = useState<undefined | MapMetadataResponse[]>(undefined);
+  const [vmin, setVMin] = useState(-500);
 
   useEffect(() => {
     async function getMapsAndMetadata() {
       const availableMaps = await fetch(`${SERVER}/maps`);
-      const availableMapsResponse = await availableMaps.json();
+      const availableMapsResponse: MapResponse[] = await availableMaps.json();
       setMaps(availableMapsResponse)
-      console.log(availableMapsResponse)
-      const availableMapsMetadata = await Promise.all(
+      const availableMapsMetadata: MapMetadataResponse[] = await Promise.all(
         availableMapsResponse.map(
-          // @ts-ignore
           async (resp) => (await fetch(`${SERVER}/maps/${resp.name}`)).json()
         )
-        // @ts-ignore
       )
-      console.log(availableMapsMetadata)
+      setMapsMetadata(availableMapsMetadata)
     }
     getMapsAndMetadata();
   }, [])
 
+  const bands = mapsMetadata ? mapsMetadata.reduce((prev: Band[], curr: MapMetadataResponse) => curr.bands ? prev.concat(curr.bands) : prev, []) : undefined;
+  console.log(bands)
+
+  const activeLayer = bands ? bands[0] : undefined;
+
   return (
-    <MapContainer id='map' {...mapOptions}>
-      {/* <TileLayer
-        url={`${SERVER}/maps/FITS_Maps/1/{z}/{y}/{x}/tile.png`}
-        tms
-        noWrap
-      /> */}
-      {/* <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      /> */}
-    </MapContainer>
+    activeLayer && mapsMetadata && (
+      <>
+        <input type='number' value={vmin} onChange={e => setVMin(Number(e.target.value))}/>
+        <MapContainer id='map' {...mapOptions}>
+          <LayersControl>
+            {bands?.map(
+                (d, i) => {
+                  const mapName = d.map_name;
+                  const mapMetadata = mapsMetadata.find(d => d.name === mapName);
+                return (
+                  <LayersControl.BaseLayer checked={i === 0} name={`${mapMetadata?.telescope} ${mapMetadata?.data_release} ${d.frequency} GHz (${d.stokes_parameter}, ${mapMetadata?.tags})`}>
+                    <TileLayer
+                      url={`${SERVER}/maps/FITS_Maps/${d.id}/{z}/{y}/{x}/tile.png?cmap=${d.recommended_cmap}&vmin=${vmin}&vmax=${d.recommended_cmap_max}`}
+                      tms
+                      noWrap
+                      bounds={
+                        latLngBounds(
+                          latLng(d.bounding_top, d.bounding_left),
+                          latLng(d.bounding_bottom, d.bounding_right),
+                        )
+                      }
+                    />
+                  </LayersControl.BaseLayer>
+                )
+              }
+            )}
+          </LayersControl>
+        </MapContainer>
+      </>
+    )
   )
 }
 
