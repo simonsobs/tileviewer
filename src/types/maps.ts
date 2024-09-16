e33c9cfc0ed6aa47f78918b00f65a24f1c636b58
Jeremy Myers <inernestmusic@gmail.com>
2024-09-16 16:20:55 -0400

    Type API responses

---
+++ src/types/maps.ts
@@ -0,0 +1,32 @@
+export type MapResponse = {
+    description: string;
+    tags: string;
+    telescope: string;
+    name: string;
+    data_release: string;
+    season: string;
+    patch: string;
+}
+
+export type Band = {
+    id: number;
+    map_name: string;
+    units: string;
+    tiles_available: boolean;
+    tile_size: number;
+    frequency: number;
+    levels: number;
+    stokes_parameter: string;
+    quantity: string;
+    bounding_top: number;
+    bounding_right: number;
+    bounding_bottom: number;
+    bounding_left: number;
+    recommended_cmap: string;
+    recommended_cmap_min: number;
+    recommended_cmap_max: number;
+}
+
+export type MapMetadataResponse = MapResponse & {
+    bands: Band[];
+}
