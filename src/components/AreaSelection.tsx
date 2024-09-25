09f195d14748841e89abf71829332800d63cd587
Jeremy Myers <inernestmusic@gmail.com>
2024-09-25 10:42:50 -0400

    Draft a different UI for 'area select' controls

--- src/components/AreaSelection.tsx
+++ src/components/AreaSelection.tsx
@@ -13,17 +13,20 @@ import { createControlComponent } from "@react-leaflet/core";
 import * as L from "leaflet";
 import './styles/area-selection.css';
 
+// creates some padding between the top-left corner of the drawn region and the overlay_pane
+const CONTROLS_CONTAINER_BUFFER = 5;
+
 interface SelectionRegionOptions extends L.ControlOptions {}
 
 class SelectionRegionControl extends L.Control {
   options: SelectionRegionOptions;
 
   base_element: HTMLElement;
+  overlay_pane: HTMLElement;
   map: L.Map;
 
   /* Buttons */
   start_button: HTMLButtonElement;
-  remove_button: HTMLButtonElement;
   passive_buttons: HTMLButtonElement[];
 
   constructor(options?: SelectionRegionOptions) {
@@ -40,57 +43,56 @@ class SelectionRegionControl extends L.Control {
     el.style.display = 'none';
   }
 
-  private showElement(el: HTMLElement, display?: string) {
-    const finalDisplay = display ?? 'block';
-    el.style.display = finalDisplay;
+  private showElement(el: HTMLElement, displayOption?: string) {
+    const display = displayOption ?? 'block';
+    el.style.display = display;
   }
 
-  private createButtons() {
+  private createPassiveButtons(container) {
     /* First create all the 'passive' buttons, those that will appear once 
      * we've selected the region and provide region-dependent functionality. */
-    const download_fits = this.createButton(this.base_element, "Download FITS");
+    const download_fits = this.createButton(container, "Download FITS");
     download_fits.addEventListener("click", (event) => {
       /* TODO: Connect these to API endpoints */
       console.log("Download FITS");
     });
 
-    const download_png = this.createButton(this.base_element, "Download PNG");
+    const download_png = this.createButton(container, "Download PNG");
     download_png.addEventListener("click", (event) => {
       /* TODO: Connect these to API endpoints */
       console.log("Download PNG");
     });
 
-    this.passive_buttons = [download_fits, download_png];
+    const remove_button = this.createButton(container, "Remove Region");
 
-    this.start_button = this.createButton(this.base_element, "Select Region");
-    this.start_button.addEventListener("click", (event) => {
-      /* The selection will be disabled by the handler once complete. */
-      this.map.selection.enable();
-      this.hideElement(this.start_button)
-      this.map.getContainer().style.cursor = "crosshair";
-    });
+    this.passive_buttons = [download_fits, download_png, remove_button];
 
-    this.remove_button = this.createButton(this.base_element, "Remove Region");
-    this.remove_button.addEventListener("click", (event) => {
+    remove_button.addEventListener("click", (event) => {
       this.map.selection.reset();
-
-      this.hideElement(this.remove_button);
-      this.passive_buttons.forEach((button) => {this.hideElement(button)});
-
-      this.showElement(this.start_button, 'flex')
-
+      this.start_button.disabled = false;
       this.options.handleSelectionBounds(undefined);
+      this.hideElement(this.overlay_pane);
     });
-
-    /* By default, hide the remove button and all passive buttons */
-    this.hideElement(this.remove_button);
-    this.passive_buttons.forEach((button) => {this.hideElement(button)});
   }
 
-  /* Mutates the state of the buttons after drawing */
-  mutateStateAfterDrawing() {
-    this.showElement(this.remove_button, 'flex');
-    this.passive_buttons.forEach((button) => {this.showElement(button, 'flex')});
+  /** 
+   * Mutates the state of the overlay pane after a selected region is drawn.
+   * More specifically, we:
+   *  1.  Make the overlay pane visible
+   *  2.  Calculate the position and size of the overlay pane wrt the bounds
+   *      of the drawn region
+  */ 
+  mutateStateAfterDrawing(bounds: L.LatLngBounds) {
+    this.showElement(this.overlay_pane);
+    const topLeft = this.map.latLngToLayerPoint(bounds.getNorthWest());
+    const bottomRight = this.map.latLngToLayerPoint(bounds.getSouthEast());
+    const width = Math.abs(bottomRight.x - topLeft.x);
+    const height = Math.abs(bottomRight.y - topLeft.y);
+    this.overlay_pane.style.top = `${topLeft.y + CONTROLS_CONTAINER_BUFFER}px`;
+    this.overlay_pane.style.left = `${topLeft.x + CONTROLS_CONTAINER_BUFFER}px`;
+    this.overlay_pane.style.width = `${width}px`;
+    this.overlay_pane.style.minWidth = 'fit-content';
+    this.overlay_pane.style.height = `${height}px`;
   }
 
   onAdd(map: L.Map) {
@@ -98,24 +100,44 @@ class SelectionRegionControl extends L.Control {
     this.map = map;
     this.map.addHandler("selection", SelectionRegionHandler);
 
-    /* First, create transparent overlay pane for the drawing handler */
-    const overlay_pane = map.createPane("overlay");
-    overlay_pane.style.zIndex = String(650);
+    /**
+     * 1. Create transparent overlay pane for the selected region's "control" elements
+     * 2. Create and attach the "control" elements to the overlay pane
+     * 3. Add event listeners that show/hide the "control" elements when mousing on/off
+     *    the overlay pane
+     * 4. Begin with the overlay pane hidden
+     */
+    this.overlay_pane = map.createPane("region-controls-overlay");
+    this.overlay_pane.style.zIndex = String(650);
+    this.createPassiveButtons(this.overlay_pane);
+    this.overlay_pane.addEventListener('mouseover', (e) => {
+      this.passive_buttons.forEach(b => this.showElement(b, 'flex'));
+    })
+    this.overlay_pane.addEventListener('mouseout', (e) => {
+      this.passive_buttons.forEach((b) => this.hideElement(b));
+    })
+    this.hideElement(this.overlay_pane);
+    /** End of notes */
 
-    /* Create container for button controls */
+    /* Create container for "Select Region" control button */
     this.base_element = L.DomUtil.create(
       "div",
       "leaflet-control-selection-region"
   );
 
-    /* Create initial elements */
-    this.createButtons();
+    /* Create start button and define its click event */
+    this.start_button = this.createButton(this.base_element, 'Select Region');
+    this.start_button.addEventListener("click", (event) => {
+      /* The selection will be disabled by the handler once complete. */
+      this.map.selection.enable();
+      this.start_button.disabled = true;
+      this.map.getContainer().style.cursor = "crosshair";
+    });
 
     /* Add callback to event handler */
-
     this.map.selection.registerCallback(
       (bounds) => {
-        this.mutateStateAfterDrawing();
+        this.mutateStateAfterDrawing(bounds);
         this.options.handleSelectionBounds(bounds)
     });
 
@@ -252,7 +274,7 @@ class SelectionRegionHandler extends L.Handler {
 
 export const AreaSelectionControl = createControlComponent(
     (props) => new SelectionRegionControl({...props, position: 'topleft'}),
-)
+  )
 
 type Props = {
   handleSelectionBounds: (bounds: L.LatLngBounds) => void;
