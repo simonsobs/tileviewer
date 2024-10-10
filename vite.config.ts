43ac8d59081c10305bb975a880d6d72aefff4352
Jeremy Myers <inernestmusic@gmail.com>
2024-10-10 16:15:15 -0400

    Adjust the vite configs to process a new build mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const baseConfig = {
   plugins: [react()],
   server: {
     host: true,
     port: 8080,
-  },
-  base: '/beta/newmaps/',
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(({mode}) => {
+  if (mode === 'production') {
+    return {
+      ...baseConfig,
+      base: '/beta/newmaps/',
+    }
+  } else if (mode === 'serve') {
+    return {
+      ...baseConfig,
+      base: '/static/',
+    }
+  } else {
+    return {
+      ...baseConfig,
+    }
+  }
 })
