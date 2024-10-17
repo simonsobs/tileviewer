50dd8de2e43c3ce9286d304e876af6016c312a31
Jeremy Myers <inernestmusic@gmail.com>
2024-10-17 15:00:02 -0400

    Remove maxBounds contraint

--- src/configs/mapSettings.ts
+++ src/configs/mapSettings.ts
@@ -1,4 +1,4 @@
-import { CRS, latLng, latLngBounds, MapOptions } from "leaflet";
+import { CRS, MapOptions } from "leaflet";
 
 export const SERVICE_URL: string = import.meta.env.VITE_SERVICE_URL || 'http://127.0.0.1:9191';
 
@@ -6,11 +6,6 @@ export const mapOptions: MapOptions = {
     center: [0.0, 0.0],
     zoom: 3,
     crs: CRS.EPSG4326,
-    maxBounds: latLngBounds(
-        latLng(-180, -90),
-        latLng(180, 90),
-    ),
-    maxBoundsViscosity: 1,
 };
 
 export const DEFAULT_MIN_ZOOM = 0;
