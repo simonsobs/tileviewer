dd2e8c2e97a0dd01e83d570d3aa8eaddab603c10
Jeremy Myers <inernestmusic@gmail.com>
2024-10-18 16:40:49 -0400

    Test configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-  dotenv.config({path: mode === 'production' ? ['.env.production'] : ['env.development']})
   return {
       plugins: [react()],
       server: {
         host: true,
         port: 8080,
       },
-      base: process.env.VITE_BASE_PATH || ''
+      base: mode === 'production' ? '/static' : ''
     }
 })
