d5881761fe80858829bae0198b7f9662c7c048fb
Jeremy Myers <inernestmusic@gmail.com>
2024-10-21 16:42:05 -0400

    Add thresholds to convert degrees to arcmin, arcsec in AstroScale

--- src/components/AstroScale.tsx
+++ src/components/AstroScale.tsx
@@ -13,25 +13,20 @@ interface AstroScaleOptions extends L.ControlOptions {
 const AstroControl = L.Control.Scale.extend({
     ...L.Control.Scale.prototype,
     _updateMetric: function() {
-	    // meters -> arcsec
         const degrees = (this.options as AstroScaleOptions).graticuleDetails.interval;
         const scaleWidth = (this.options as AstroScaleOptions).graticuleDetails.pixelWidth;
         let unitName = 'deg';
-	    // var maxArcsec = maxMeters / 30.87;
-	    // var maxUnit = maxArcsec;
-	    // var unitName = 'arcsec';
-	    // if (maxArcsec > 7200) {
-	    //     // degrees
-	    //     maxUnit /= 3600;
-	    //     unitName = 'deg';
-	    // } else if (maxArcsec >= 180) {
-	    //     // arcmin
-	    //     maxUnit /= 60;
-	    //     unitName = 'arcmin';
-	    // }
-	    // var unit = (this as any)._getRoundNum(maxUnit);
+        let value = degrees; 
+
+        if (degrees < 1/60) {
+            value = degrees * 60 * 60;
+            unitName = 'arcsec'
+        } else if (degrees < 1) {
+            value = degrees * 60;
+            unitName = 'arcmin'
+        }
 		(this as any)._mScale.style.width = scaleWidth + 'px';
-	    (this as any)._mScale.innerHTML = degrees + ' ' + unitName;
+	    (this as any)._mScale.innerHTML = value + ' ' + unitName;
     }
 })
 
