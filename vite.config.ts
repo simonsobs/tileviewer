deaf9deb0edee074959be3edb7cfc0064330268f
Jeremy Myers <inernestmusic@gmail.com>
2024-10-22 13:34:40 -0400

    Allow for production-level args

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
+  dotenv.config({ path: mode === 'production' ? ['.env.production'] : undefined})
   return {
       plugins: [react()],
       server: {
         host: true,
         port: 8080,
       },
-      base: mode === 'production' ? '/static' : ''
+      base: mode === 'production' ? (
+        process.env.VITE_BASE_PATH || '/static'
+      ) : ''
     }
 })
