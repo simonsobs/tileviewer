17c4604694b16d4e305e9cf916adacde4ce922cf
Jeremy Myers <inernestmusic@gmail.com>
2025-01-28 15:21:03 -0500

    Add stokes_param to layer name, if present

--- src/utils/layerUtils.ts
+++ src/utils/layerUtils.ts
@@ -3,9 +3,11 @@ import { Band } from "../types/maps";
 /**
  * A utility function to format a layer's name.
  * @param band The band object
- * @returns A string of map_name + quantity, where quantity is conditionally
- *          rendered based on its truthiness
+ * @returns A string of map_name + stokes_parameter + quantity, where quantity 
+ *  and stokes_parameter are conditionally rendered based on their truthiness
  */
 export function makeLayerName(band: Band) {
-    return band.map_name + (band.quantity ? ` ${band.quantity}` : '')
+    return band.map_name + 
+        (band.stokes_parameter ? ` ${band.stokes_parameter}` : '') + 
+        (band.quantity ? ` ${band.quantity}` : '')
 }
