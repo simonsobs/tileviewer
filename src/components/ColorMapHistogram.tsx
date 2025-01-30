9dcb766856da201b145561df8b2d4784c07d548f
Jeremy Myers <inernestmusic@gmail.com>
2025-01-30 10:40:07 -0500

    Fix bug in histogram polygon creation

--- src/components/ColorMapHistogram.tsx
+++ src/components/ColorMapHistogram.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import {
     PointArray,
   } from "@svgdotjs/svg.js";
@@ -15,42 +16,60 @@ type Props = {
 
 export function ColorMapHistogram({data, userMinAndMaxValues}: Props) {
     const { edges, histogram } = data;
-    /** Convert the histogram into log values using the safeLog utility function */
-    const logarithmicHistogram = histogram.map(safeLog)
-    /** Include user's min value in case we need to rescale the histogram accordingly */
-    const edgeStart = Math.min(...edges, userMinAndMaxValues.min);
-    /** Include user's max value in case we need to rescale the histogram accordingly */
-    const edgeEnd = Math.max(...edges, userMinAndMaxValues.max);
-    
-    const histogramStart = Math.min(...logarithmicHistogram);
-    // Add a little buffer; we don't want the histogram to touch the top of the image
-    const histogramEnd = Math.max(...logarithmicHistogram) * 1.05;
 
-    const polygon = generatePolygon(
-        edges.map((x) => {
-          return translateDatum(x, edgeStart, edgeEnd, HISTOGRAM_SIZE_X);
-        }),
-        logarithmicHistogram.map((y) => {
-          return translateDatum(y, histogramStart, histogramEnd, HISTOGRAM_SIZE_Y);
-        }),
-        HISTOGRAM_SIZE_X,
-        HISTOGRAM_SIZE_Y,
-      );
+    /**
+     * The polygon constructed for use as the SVG for the histogram.
+     * We memoized polygon so that it's only generated when the
+     * props change.
+     */
+    const polygon = useMemo(
+      () => {
+        /** Convert the histogram into log values using the safeLog utility function */
+        const logarithmicHistogram = histogram.map(safeLog)
+        /** Include user's min value in case we need to rescale the histogram accordingly */
+        const edgeStart = Math.min(...edges, userMinAndMaxValues.min);
+        /** Include user's max value in case we need to rescale the histogram accordingly */
+        const edgeEnd = Math.max(...edges, userMinAndMaxValues.max);
+        
+        const histogramStart = Math.min(...logarithmicHistogram);
+        // Add a little buffer; we don't want the histogram to touch the top of the image
+        const histogramEnd = Math.max(...logarithmicHistogram) * 1.05;
+
+        return generatePolygon(
+          edges.map((x) => {
+            return translateDatum(x, edgeStart, edgeEnd, HISTOGRAM_SIZE_X);
+          }),
+          logarithmicHistogram.map((y) => {
+            return translateDatum(y, histogramStart, histogramEnd, HISTOGRAM_SIZE_Y);
+          }),
+          HISTOGRAM_SIZE_X,
+          HISTOGRAM_SIZE_Y,
+        );
+      }, [edges, histogram, userMinAndMaxValues.min, userMinAndMaxValues.max]
+    )
 
     return (
-        <svg
-            className="cmap-histogram"
-            width={HISTOGRAM_SIZE_X}
-            height={HISTOGRAM_SIZE_Y}>
-            <polyline
-                opacity={0.65}
-                fill="black"
-                points={polygon.toString()}
-            />
-        </svg>
+      <svg
+        className="cmap-histogram"
+        width={HISTOGRAM_SIZE_X}
+        height={HISTOGRAM_SIZE_Y}>
+        <polyline
+            opacity={0.65}
+            fill="black"
+            points={polygon.toString()}
+        />
+      </svg>
     )
 }
 
+/**
+ * Generates a polygon to be rendered as a histogram SVG
+ * @param edges Edges of the histogram
+ * @param histogram Values of the histogram
+ * @param size_x Fixed width of the histogram
+ * @param size_y Fixed height of the histogram
+ * @returns PointArray used to render a polyline SVG
+ */
 function generatePolygon(
     edges: number[],
     histogram: number[],
@@ -61,7 +80,7 @@ function generatePolygon(
 
     /* First point is the bottom left corner */
     polygon.push(0, size_y);
-    polygon.push(histogram[0], size_y);
+    polygon.push(edges[0], size_y - histogram[0]);
 
     for (let i = 0; i <= histogram.length - 1; i++) {
       polygon.push(edges[i], size_y - histogram[i]);
@@ -69,13 +88,21 @@ function generatePolygon(
     }
 
     /* Last point is bottom right corner, then wrap back around to the start. */
-    polygon.push(edges[edges.length - 1], size_y);
+    polygon.push(edges[edges.length - 1], size_y - histogram[histogram.length - 1]);
     polygon.push(size_x, size_y);
     polygon.push(0, size_y);
 
     return new PointArray(polygon);
   };
 
+  /**
+   * Scales the edge and histogram data according to a provided fixed size
+   * @param data The edge or histogram data point
+   * @param min Min edge or histogram value
+   * @param max Max edge or histogram value
+   * @param size Fixed width or height of the histogram
+   * @returns A scaled value
+   */
   function translateDatum(data: number, min: number, max: number, size: number): number {
     return ((data - min) / (max - min)) * size;
   }
