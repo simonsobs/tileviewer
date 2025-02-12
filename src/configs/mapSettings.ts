d3a5bf22e847733c2ea463f74183da9754bfa790
Jeremy Myers <inernestmusic@gmail.com>
2025-02-12 16:03:42 -0500

    Remove end forward slash from window.location.href

--- src/configs/mapSettings.ts
+++ src/configs/mapSettings.ts
@@ -1,7 +1,7 @@
 // Sets the SERVICE_URL used in the fetch endpoints to a VITE_SERVICE_URL variable
 // defined in a .env file or otherwise falls back to window.location.href
 export const SERVICE_URL: string =
-  import.meta.env.VITE_SERVICE_URL || window.location.href;
+  import.meta.env.VITE_SERVICE_URL || window.location.href.slice(0, -1);
 
 // Sets the number of decimals used in the coordinates display
 export const NUMBER_OF_FIXED_COORDINATE_DECIMALS = 5;
