8e2e575975a30602b2e4d3efdb43b840e14c1598
Jeremy Myers <inernestmusic@gmail.com>
2025-04-10 15:23:54 -0400

    Refactor CoordinatesDisplay for OpenLayers

--- src/components/CoordinatesDisplay.tsx
+++ src/components/CoordinatesDisplay.tsx
@@ -1,51 +1,17 @@
-import { useMap } from 'react-leaflet';
-import { LatLng, LeafletMouseEvent } from 'leaflet';
-import { useCallback, useEffect, useState } from 'react';
 import { NUMBER_OF_FIXED_COORDINATE_DECIMALS } from '../configs/mapSettings';
 import './styles/coordinates-display.css';
 
-/**
- * QUESTION: Would this be better as an extended L.Control?
- */
-
-export function CoordinatesDisplay() {
-  const [latLng, setLatLng] = useState<LatLng | undefined>(undefined);
-  const map = useMap();
-
-  const handleMouseMove = useCallback(
-    (e: LeafletMouseEvent) => {
-      setLatLng(e.latlng);
-    },
-    [setLatLng]
-  );
-
-  const handleMouseOut = useCallback(
-    () => (latLng ? setLatLng(undefined) : null),
-    [latLng, setLatLng]
-  );
-
-  useEffect(() => {
-    map.on('mousemove', handleMouseMove);
-    map.on('mouseout', handleMouseOut);
-
-    return function cleanup() {
-      map.off('mousemove', handleMouseMove);
-      map.off('mouseout', handleMouseOut);
-    };
-  }, [map, handleMouseMove, handleMouseOut]);
-
+export function CoordinatesDisplay({ coordinates }: { coordinates: number[] }) {
   return (
-    latLng && (
-      <div className="coordinates-display">
-        <span className="parens">( </span>
-        <span className="coords lat">
-          {latLng?.lat.toFixed(NUMBER_OF_FIXED_COORDINATE_DECIMALS)} ,
-        </span>
-        <span className="coords lng">
-          {latLng?.lng.toFixed(NUMBER_OF_FIXED_COORDINATE_DECIMALS)}
-        </span>
-        <span className="parens"> )</span>
-      </div>
-    )
+    <div className="coordinates-display">
+      <span className="parens">( </span>
+      <span className="coords lat">
+        {coordinates[0].toFixed(NUMBER_OF_FIXED_COORDINATE_DECIMALS)} ,
+      </span>
+      <span className="coords lng">
+        {coordinates[1].toFixed(NUMBER_OF_FIXED_COORDINATE_DECIMALS)}
+      </span>
+      <span className="parens"> )</span>
+    </div>
   );
 }
