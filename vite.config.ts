148098406171f71e1679f15a6e1401399b7e48b1
Jeremy Myers <inernestmusic@gmail.com>
2025-08-25 14:10:51 -0400

    Add vitest to test utility fxns

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -10,5 +11,8 @@ export default defineConfig(({ mode }) => {
       port: 8080,
     },
     base: mode === 'production' ? './' : '',
+    test: {
+      // ... Specify options here.
+    },
   };
 });
