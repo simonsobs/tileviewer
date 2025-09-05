01ac0c747d5153ab2da1aa86164c606e6c0bebd0
Jeremy Myers <inernestmusic@gmail.com>
2025-09-05 16:00:35 -0400

    Create docs w/ Read The Docs

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Tileviewer'
+copyright = '2025, Jeremy Myers, Josh Borrow'
+author = 'Jeremy Myers, Josh Borrow'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_css_files = ["custom.css"]
+
+# Add myst_parser so Sphinx can read Markdown
+extensions = [
+    "myst_parser",
+]
+
+# Tell Sphinx to treat .md files as source files
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
